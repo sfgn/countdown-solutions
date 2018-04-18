cd794257b6f94903fe30c48c56998b3e92ba2016
Scott Flack <hello@scottflack.com>
2018-04-18 22:28:25 +1000

    Initial commit of files

---
+++ index.js
@@ -0,0 +1,77 @@
+let fs = require('fs');
+let path = require('path');
+
+function swap(chars, i, j) {
+    var tmp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = tmp;
+}
+
+function getCombinations(chars) {
+  const length = 9;
+  let counter = [];
+  let combinations = [];
+  let i;
+
+  for (i = 0; i < length; i++) {
+      counter[i] = 0;
+  }
+
+  i = 0;
+  combinations.push(chars.join(''));
+  while (i < length) {
+      if (counter[i] < i) {
+          swap(chars, i % 2 === 1 ? counter[i] : 0, i);
+          counter[i]++;
+          i = 0;
+          combinations.push(chars.join(''));
+      } else {
+          counter[i] = 0;
+          i++;
+      }
+  }
+
+  return combinations;
+}
+
+let dictionary = fs.readFileSync(path.dirname(require.main.filename) + '/dictionary.txt').toString().split("\r\n");
+
+var stdin = process.openStdin();
+console.log('What are the letters? ');
+
+stdin.addListener("data", function(d) {
+  let letters =  d.toString().trim().split('');
+  let words = getCombinations(letters);
+  let wordList = {};
+  let output = {};
+
+  for (w in dictionary) {
+    wordList[dictionary[w].toLowerCase()] = dictionary[w];
+  }
+
+  for (w in words) {
+    let word = '';
+
+    for(i = 0; i < words[w].length; i++) {
+      word += words[w][i];
+      if (word.length >= 4 && typeof wordList[word] !== 'undefined') {
+        output[word] = word;
+      }
+    }
+  }
+
+  let realWords = Object.keys(output).map(val => {
+    return val;
+  });
+
+  realWords = realWords.sort((a,b) => {
+    return a.length - b.length;
+  });
+
+
+  realWords.map(word => {
+    console.log(word.length + ': ' + word);
+  });
+
+  process.exit(0);
+});
